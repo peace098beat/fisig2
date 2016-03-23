31caa11de5648ee87937ec3707f35afc16c7023e
peace098beat <peace098beat.t@gmail.com>
2016-03-23 22:30:07 +0900

    add litelistview()

--- gui/demo_MDI.py
+++ gui/demo_MDI.py
@@ -6,12 +6,15 @@ Created by 0160929 on 2016/03/23 16:03
 """
 import sys
 
-from PySide.QtCore import Slot
+from PySide.QtCore import Slot, Qt
 from PySide.QtGui import QMainWindow, QApplication, QWorkspace, QVBoxLayout, QPushButton, QWidget, \
     QStatusBar, QProgressBar
 
-from gui.guitools.FileListWidget import FileListView
-from fisig import SignalData
+from fisig2 import SignalData
+from gui.guitools.LiteListView import LiteListView
+
+CSS_PATH = "./guitools/style13.css"
+
 
 class MyMDIApp(QMainWindow):
     def __init__(self):
@@ -27,11 +30,11 @@ class MyMDIApp(QMainWindow):
         workspace = QWorkspace()
         workspace.setWindowTitle("Simple WorkSpace Exmaple")
 
-        # :: FileListView
-        self.file_view = FileListView()
-        self.file_view.resize(150, 300)
-        self.file_view.setWindowTitle("File List")
-        workspace.addWindow(self.file_view)
+        # :: lite_listview
+        self.lite_listview = LiteListView()
+        self.lite_listview.addItems(["abc", "def", "ghi", "jkl"])
+        self.lite_listview.resize(150, 300)
+        workspace.addWindow(self.lite_listview)
 
         # :: ボタンs
         layout = QVBoxLayout()
@@ -50,9 +53,7 @@ class MyMDIApp(QMainWindow):
         self.myStatusBar = QStatusBar()
         self.setStatusBar(self.myStatusBar)
         self.myStatusBar.showMessage('Ready', 1000)
-        # ====================================
-        # ステータスバーにプログラスバーを追加
-        # ====================================
+        # :: ステータスバーにプログラスバーを追加
         self.progressBar = QProgressBar()
         self.myStatusBar.addPermanentWidget(self.progressBar)
         self.progressBar.reset()
@@ -67,26 +68,25 @@ class MyMDIApp(QMainWindow):
         # ====================================
         # シグナルスロットのコネクト
         # ====================================
-        # FileListViewの関数
-        self.file_view.fileDropped.connect(self.set_filelist)
-        self.file_view.clicked.connect(self.slot1)
+        # lite_listview
+        self.lite_listview.fileDropped.connect(self.set_filelist)
+        self.lite_listview.clicked.connect(self.slot1)
         # Analysisボタンで解析実行
         self.btn_ana.clicked.connect(self.run_analysis)
         self.btn_plot.clicked.connect(self.run_plot)
 
+
     @Slot()
     def slot1(self, index):
-        # print 'listslot', index.row(), index.data(Qt.DisplayRole)
-        # print self.filelist[index.row()]
-        s = "Clicked : " + self.filelist[index.row()]
+        # print index.row()
+        selected_stritem = index.data(Qt.DisplayRole)
+        s = "Clicked[%d] : %r" %(index.row() , selected_stritem)
         self.flush(s)
-        pass
 
     @Slot()
     def set_filelist(self, file_list):
         self.filelist = file_list
         self.flush("Load File List")
-        pass
 
     @Slot()
     def run_analysis(self):
@@ -96,15 +96,14 @@ class MyMDIApp(QMainWindow):
 
         N = len(self.filelist)
 
-        result=[]
+        result = []
 
         for i, wav_file in enumerate(self.filelist):
-
             # 解析
-            gawa,katasa = demo_analysis(wav_file)
-            result.append([gawa,katasa])
+            gawa, katasa = demo_analysis(wav_file)
+            result.append([gawa, katasa])
             # プログレスバー
-            self.progressBar.setValue(float(i+1)/N*100)
+            self.progressBar.setValue(float(i + 1) / N * 100)
 
         self.progressBar.setVisible(False)
         self.result = result
@@ -114,13 +113,17 @@ class MyMDIApp(QMainWindow):
         pass
 
     @Slot()
-    def flush(self, s):
-        self.myStatusBar.showMessage(s, 1 * 1000)
+    def flush(self, s, ms=1):
+        maxlen = 50
+        if len(s) > maxlen:
+            s = s[:10] + "..." + s[-maxlen:]
+        self.myStatusBar.showMessage(s, ms * 1000)
 
     def setCSS(self):
         """cssを読みこんでセット        """
-        with open("./guimodule/style13.css", "r") as f:
+        with open(CSS_PATH, "r") as f:
             self.setStyleSheet("".join(f.readlines()))
+
 
 def demo_analysis(wavfilepaht):
     # オーディオロード
